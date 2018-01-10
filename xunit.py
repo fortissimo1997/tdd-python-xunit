7f4e19a4e21798f5d192f795ca5747dc8d3d5307
fortissimo1997 <fortissimo1997@gmail.com>
2018-01-10 22:08:28 +0900

    TDD@p176

--- xunit.py
+++ xunit.py
@@ -33,6 +33,11 @@ class TestSuite:
         self.tests = []
     def add(self, test):
         self.tests.append(test)
+    def run(self):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+        return result
 
 class WasRun(TestCase):
     def setUp(self):
